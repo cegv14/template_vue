55ad81a4d9ad8368a91ba6807930dfc1c4e4be15
Cristian <cristian7368@gmail.com>
2025-01-26 20:05:19 -0400

    cambios

--- src/main.js
+++ src/main.js
@@ -2,22 +2,24 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import * as ElementPlusIconsVue from '@element-plus/icons-vue' // Importa todos los iconos
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css' // Importa el CSS
 
+import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
+import './assets/styles/tailwind.css'
 import App from './App.vue'
 import router from './router'
 
 const app = createApp(App)
 
-// Registra los iconos globalmente
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
   app.component(key, component)
 }
 
 app.use(createPinia())
+
 app.use(router)
-app.use(ElementPlus) // Usa Element Plus
+app.use(ElementPlus)
 
 app.mount('#app')
